2c94e4c26e4a00ec66c8a8a0239fe2c0f28178e3
Ivandro Neto <ivandroneto1@outlook.com>
2024-05-01 19:45:46 +0100

    Adding Home Page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,66 @@
+'use client';
 import Image from "next/image";
+import { NavBar } from "./components/Navbar";
+import Cards from "./components/Cards";
 import styles from "./page.module.css";
-
+import MenuFooter from "./components/MenuFooter";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.screen}>
+      <NavBar/>
+    <main className={styles.container}>
+      <section className={styles.welcome}>
+        <h3 className={styles.welcome_title}>Olá, Francisca Tungumuna</h3>
+        <p className={styles.welcome_description}>Teste as suas habilidades. Escolha o seu desafio!</p>
+        <span className={styles.hastag}>#StartupSummit2024</span>
+      </section>
+      <section className={styles.cards_container}>
+        <div className={styles.rowcard}>
+          <Cards 
+            url={"/Puzzle.svg"}
+            iconColor = {"#5CB0A9"}
+            ySize={"340px"} 
+            title={"Caso de Estudo"} 
+            description={"Soluções criativas para desafios reais"}
+            bgColor={"#305350"}
+            fgColor={"whitesmoke"} 
+            gap="20px"
+            img="block"
+          />
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className={styles.side_action}>
+          <Cards 
+          url={"/LightOn.svg"}
+          iconColor = {"#D2A340"}
+          ySize={"50%"} 
+          title={"Perguntas Múltiplas"} 
+          bgColor={"#FFEAB9"}
+          fgColor={"#D2A340"} 
+          />
+          <Cards 
+          url={"/People.svg"}
+          iconColor = {"#FFEAB9"}
+          ySize={"50%"} 
+          title={"Desafio Multiplayer"}
+          bgColor={"#D2A340"}
+          fgColor={"whitesmoke"} 
+          />
+        </div>
+      </section>
+      <section className={styles.others}>
+        <h1>Outros</h1>
+        <button className={styles.fullbtn}>
+          <Image 
+            src={"Warranty.svg"}
+            alt="Warranty"
+            width={48}
+            height={48} 
+          / >
+          Visualizar prêmios disponíveis
+          </button>
+      </section>
     </main>
+      <MenuFooter/>
+    </div>
   );
 }
